4c0ea747ffb2f61ca33131aaf347f1a7437eeb4d
bechampion <jgarcia@Jeronimos-Mac-mini.local>
2014-08-25 20:06:54 -0300

    removing drush to avoid a database drop

--- manifests/init.pp
+++ manifests/init.pp
@@ -53,39 +53,4 @@ class drupal (
     target  => '/home/vagrant/drush/drush.php',
     require => File['/home/vagrant/drush/drush.php'],
   }
-
-  # Construct Drush options
-  $a_m   = "--account-mail=${account_mail}"
-  $a_n   = "--account-name=${account_name}"
-  $a_p   = "--account-pass=${account_pass}"
-  $c_u   = "--clean-url=${clean_url}"
-  $d_p   = $db_prefix ? {
-    false   => '',
-    default => "--db-prefix=${db_prefix}"
-  }
-  $d_s   = $db_su ? {
-    false   => '',
-    default => "--db-su=${db_su}"
-  }
-  $d_s_p = $db_su_pw ? {
-    false   => '',
-    default => "--db-su-pw=${db_su_pw}"
-  }
-  $d_u   = "--db-url=${db_type}://${db_user}:${db_pass}@${db_host}:${db_port}/${db_name}"
-  $l     = "--locale=${locale}"
-  $s_m   = "--site-mail=${site_mail}"
-  $s_n   = "--site-name=${site_name}"
-  $s_s   = "--sites-subdir=${sites_subdir}"
-
-  $site_install = "${a_m} ${a_n} ${a_p} ${c_u} ${d_p} ${d_s} ${d_s_p} ${d_u} ${l} ${s_m} ${s_n} ${s_s}"
-
-  exec { "touch ${install_location}/drush-run && yes | drush site-install ${site_install}":
-    creates => "${install_location}/drush-run",
-    path    => ['/bin', '/usr/bin', '/usr/local/bin'],
-    cwd     => $install_location,
-    require => [
-      Package['php'],
-      File['/usr/local/bin/drush']
-    ],
-  }
 }
